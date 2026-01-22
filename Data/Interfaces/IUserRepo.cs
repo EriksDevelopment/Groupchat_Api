619515d65b5365d6e587587b251651a1c32c0a04
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-22 22:37:20 +0100

    Implemented user interface

---
+++ Data/Interfaces/IUserRepo.cs
@@ -0,0 +1,10 @@
+using Groupchat_Api.Data.Models;
+
+namespace Groupchat_Api.Data.Interfaces
+{
+    public interface IUserRepo
+    {
+        Task<User> AddUserAsync(User user);
+        Task<User> UserNameExistsAsync(string userName);
+    }
+}
