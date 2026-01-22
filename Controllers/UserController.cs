16de8e4c63baa8edc3d8d449d8f6e84809bb022a
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-22 23:07:53 +0100

    Implemented user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,42 @@
+using Groupchat_Api.Core.Interfaces;
+using Groupchat_Api.Data.Dtos.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Groupchat_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ILogger<UserController> _logger;
+        public UserController(IUserService userService, ILogger<UserController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisterResponseDto>> Register([FromBody] RegisterRequestDto dto)
+        {
+            try
+            {
+                var result = await _userService.RegisterAsync(dto);
+
+                _logger.LogInformation("Registration successfull.");
+                return Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while registering user.");
+                return StatusCode(500, "Something went wrong.");
+            }
+        }
+    }
+}
