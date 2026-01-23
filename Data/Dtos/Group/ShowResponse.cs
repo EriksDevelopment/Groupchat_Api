a24401637dc6455b6f753a5d41362814ae56423c
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-23 18:14:49 +0100

    Implemented dto for show groups

---
+++ Data/Dtos/Group/ShowResponse.cs
@@ -0,0 +1,11 @@
+namespace Groupchat_Api.Data.Dtos
+{
+    public class ShowResponseDto
+    {
+        public string GroupName { get; set; } = null!;
+        public string? Bio { get; set; }
+        public int Members { get; set; }
+        public string InviteCode { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
