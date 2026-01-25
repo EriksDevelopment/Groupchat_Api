b2709769423f57c5dae4506c0ae99ab6a796cd5d
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-25 01:01:59 +0100

    Implemented dto

---
+++ Data/Dtos/Message/MessageDeleteRequestDto.cs
@@ -0,0 +1,7 @@
+namespace Groupchat_Api.Data.Dtos.Message
+{
+    public class MessageDeleteRequestDto
+    {
+        public string MessageCode { get; set; } = null!;
+    }
+}
---
+++ Data/Dtos/Message/MessageDeleteResponseDto.cs
@@ -0,0 +1,7 @@
+namespace Groupchat_Api.Data.Dtos.Message
+{
+    public class MessageDeleteResponseDto
+    {
+        public string Message { get; set; } = null!;
+    }
+}
